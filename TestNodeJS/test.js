5adc216fa999dfadca6e397423ef06de1e17c2e7
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-06-08 21:58:19 +0530

    learning NPM

--- TestNodeJS/test.js
+++ TestNodeJS/test.js
@@ -1,3 +1,11 @@
 const importedSecondModule = require("./second.js")
 
 console.log(importedSecondModule.myName);
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
