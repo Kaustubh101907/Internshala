bc7387d3bb7235fb9950f6853c8d8427283a8901
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-06-22 20:46:46 +0530

    internshalla

---
+++ MordenJS/Let&Const.js
@@ -0,0 +1,10 @@
+function printMyName() {
+    if (5 > 1) {
+        if (10 < 20) {
+            let myName = "Kaustubh";  // visiblity ends whenever we go outside the { }
+            console.log(myName);
+        }
+    }
+}
+
+printMyName();
---
+++ MordenJS/varExample.js
@@ -0,0 +1,10 @@
+function printMyName() {
+    if (5 < 10) {
+        if (10 > 2) {
+            var myName = "Kaustubh"; // visibility is everywhere inside the function
+        }
+    }
+    console.log(myName);
+}
+
+printMyName();
