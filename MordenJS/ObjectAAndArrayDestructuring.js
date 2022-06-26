bf922892a293fd8d77d95e9be0757045a22e6515
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-06-26 20:47:16 +0530

    it

--- MordenJS/ObjectAAndArrayDestructuring.js
+++ MordenJS/ObjectAAndArrayDestructuring.js
@@ -1,3 +1,4 @@
+// Object destructuring
 const car = {
     color: "red",
     gears: 5,
@@ -9,8 +10,16 @@ const car = {
 // const gears = car.gears;
 // const color = car.color;
 
-const {color, gears, engine} = car;
+const {color, gears, engine: carEngine} = car;
 
 console.log("color", color);
 console.log("gears", gears);
-console.log("engine", engine);
+//console.log("engine", engine); // can do it like this also
+console.log("engine", carEngine); // naming another term with it
+
+// Array destructuring
+const someArr = [4,3,1,5,6];
+
+const [f, s, t] = someArr;
+
+console.log(f, s, t);
