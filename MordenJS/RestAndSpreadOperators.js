c1808d848912f8c1ff185f8635291f9c8c4794b9
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-06-26 23:11:09 +0530

    internshalla

--- MordenJS/RestAndSpreadOperators.js
+++ MordenJS/RestAndSpreadOperators.js
@@ -1,10 +1,15 @@
 // rest = accumulates or folds values into a single variable
+// spread = unfolds values
 
-// function someFunc(arg1, arg2, ...restOfArgs) {
-//     console.log(restOfArgs);
-// }
+// Example 1
 
-// someFunc("hello", "Andrew", 15, () => {}, 5);
+function someFunc(arg1, arg2, ...restOfArgs) {
+    console.log(restOfArgs);
+}
+
+someFunc("hello", "Andrew", 15, () => {}, 5);
+
+// Example 2
 
 const person = {
     name: "Andrew",
@@ -16,3 +21,23 @@ const {name, ...restOfFeatures} = person;
 
 console.log(name);
 console.log(restOfFeatures);
+
+// Example 3
+
+const someArr = [5,4,3,10,7];
+
+const [f, s, ...rest] = someArr;
+
+console.log(rest)
+
+// Spread
+// for example we need to merge the person obj and a new obj other
+
+const other = {
+    name2: "Alex",
+    car: "BMW",
+};
+
+const merge = {...person, ...other};
+
+console.log(merge);
